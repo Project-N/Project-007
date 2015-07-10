1de56db5512cfc3c01aacf14ad6ecbc0ff259d8c
Trevor Jex <jextrevor@gmail.com>
2015-07-10 12:20:51 -0600

    Low vs High Accuracy

--- client/Project-007.js
+++ client/Project-007.js
@@ -1,10 +1,15 @@
 if (Meteor.isClient) {
     $(document).ready(function() {
-        if (typeof navigator == 'undefined') {
-            console.log('Error!');
-        } else {
-            navigator.geolocation.getCurrentPosition(function(position) {
-                console.log(position.coords.latitude + ' ' + position.coords.longitude + ' ' + position.coords.accuracy);
+            
+            setInterval(getLocation,500);
+
+          
+        
+    });
+    function getLocation() {
+        navigator.geolocation.getCurrentPosition(function(position) {
+                document.getElementById('currentLat').innerHTML = position.coords.latitude.toString();
+            document.getElementById('currentLong').innerHTML = position.coords.longitude.toString();
             }, function(error) {
                 alert('Error occurred. Error code: ' + error.code);
                 // error.code can be:
@@ -12,17 +17,21 @@ if (Meteor.isClient) {
                 //   1: permission denied
                 //   2: position unavailable (error response from locaton provider)
                 //   3: timed out
-            });
-            setInterval(getLocation,500);
-
-          
-        }
-    });
-    function getLocation() {
-        var position = Geolocation.currentLocation();
-        if (!(typeof position == 'undefined' || position === null)) {
-            document.getElementById('currentLat').innerHTML = position.coords.latitude.toString();
-            document.getElementById('currentLong').innerHTML = position.coords.longitude.toString();
-        }
+            },{enableHighAccuracy:true,maximumAge:0});
+        navigator.geolocation.getCurrentPosition(function(position) {
+                document.getElementById('currentLat2').innerHTML = position.coords.latitude.toString();
+            document.getElementById('currentLong2').innerHTML = position.coords.longitude.toString();
+            }, function(error) {
+                alert('Error occurred. Error code: ' + error.code);
+                // error.code can be:
+                //   0: unknown error
+                //   1: permission denied
+                //   2: position unavailable (error response from locaton provider)
+                //   3: timed out
+            },{enableHighAccuracy:false,maximumAge:0});
+        //if (!(typeof position == 'undefined' || position === null)) {
+        //    document.getElementById('currentLat').innerHTML = position.coords.latitude.toString();
+        //    document.getElementById('currentLong').innerHTML = position.coords.longitude.toString();
+        //}
     }
 }
