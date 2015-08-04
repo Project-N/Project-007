e2b62110505bd26cd863d04d06aeb572210089ae
Cayden Lund <caydenlund@gmail.com>
2015-08-04 10:52:43 -0600

    Finalizing Players Collection methods.

--- methods.js
+++ methods.js
@@ -1,7 +1,23 @@
 Meteor.methods({
-    newPlayer: function(id, lat, long, speed, heading, accuracy) {
-        if (typeof id != 'undefined' && typeof Players.findOne({id: id}) == 'undefined') {
-            Players.insert({id: id, position: {
+    newPlayer: function(id, team, lat, long, speed, heading, accuracy) {
+        if (typeof id !== 'undefined' && typeof Players.findOne({id: id}) === 'undefined') {
+            Players.insert({
+                id: id,
+                team: team,
+                position: {
+                    latitude: lat,
+                    longitude: long,
+                    speed: speed,
+                    heading: heading,
+                    accuracy: accuracy
+                }
+            });
+        }
+        return id;
+    },
+    updatePlayer: function(id, lat, long, speed, heading, accuracy) {
+        if (typeof id !== 'undefined' && typeof Players.findOne({id: id}) === 'undefined') {
+            Players.update(Players.findOne({id: id}), { position: {
                 latitude: lat,
                 longitude: long,
                 speed: speed,
@@ -12,7 +28,7 @@ Meteor.methods({
         return id;
     },
     removePlayer: function(id) {
-        if (typeof id != 'undefined' && typeof Players.findOne({id: id}) != 'undefined') {
+        if (typeof id !== 'undefined' && typeof Players.findOne({id: id}) !== 'undefined') {
             Players.remove(Players.findOne({id: id}));
         }
         return id;
