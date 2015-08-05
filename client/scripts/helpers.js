e28896cf989ffea8a8bc1e48edb6dbb869f324eb
Trevor Jex <jextrevor@gmail.com>
2015-08-05 17:00:02 -0600

    Updated interface - UNSTABLE

--- client/scripts/helpers.js
+++ client/scripts/helpers.js
@@ -15,18 +15,20 @@ Template.menubutton.events({
         else{
         	document.getElementById("topbar").style.visibility = "hidden";
         }*/
-        if(document.getElementById("bottombar").style.visibility == "hidden"){
+        /*if(document.getElementById("bottombar").style.visibility == "hidden"){
         	document.getElementById("bottombar").style.visibility = "visible";
         }
         else{
         	document.getElementById("bottombar").style.visibility = "hidden";
-        }
+        }*/
         if(Session.get("open") == false || document.getElementById("menu").style.left == "-100%"){
         	document.getElementById("menu").style.left = "0px";
+            document.getElementById("bottombar").style.bottom = "-72px";
         	Session.set("open",true);
         }
         else{
         	document.getElementById("menu").style.left = "-100%";
+            document.getElementById("bottombar").style.bottom = "0px";
         	Session.set("open",false);
         }
     }
@@ -49,6 +51,12 @@ Template.viewport.helpers({
 	screenheight:function(){
 		return window.innerHeight - 144;
 	},
+    youx:function(){
+        return (window.innerWidth/2)-13;
+    },
+    youy:function(){
+        return ((window.innerHeight-144)/2)-1052.3622;
+    },
     players:Players.find().fetch()
 });
 Template.dot.helpers({
