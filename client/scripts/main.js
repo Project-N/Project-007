1838d1da02d5283ae5d8a10c9822ca3c4ea57d7f
Cayden Lund <caydenlund@gmail.com>
2015-08-05 18:29:07 -0600

    [UNSTABLE] Adding database integration.

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -11,6 +11,15 @@ $(document).ready(function() {
 
         Session.set("open",false);
 
+    var id = Math.random().toString(36).substring(2);
+    Session.set('id', id);
+    console.log(id);
+    Meteor.call('newPlayer', id, 'Team 1', 0, 0, 0, 0, 0);
+    watchPosition();
+
+});
+$(window).bind('beforeunload', function(){
+  Meteor.call('removePlayer',Session.get('id'));
 });
 window.requestAnimFrame = function(){
     return (
@@ -24,25 +33,52 @@ window.requestAnimFrame = function(){
         }
     );
 }();
-    function getLocation() {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            //document.getElementById('currentLat').innerHTML = position.coords.latitude.toString();
-            //document.getElementById('currentLong').innerHTML = position.coords.longitude.toString();
-        }, function(error) {
-            //alert('Error occurred. Error code: ' + error.code);
-            if(error.code == 1){
-                alert("Please turn on Location Services and GPS, and allow your app to use them.");
-            }
-            if(error.code == 2){
-                //Location unavailable - you go into gray mode.
-            }
-            // error.code can be:
-            //   0: unknown error
-            //   1: permission denied
-            //   2: position unavailable (error response from location provider)
-            //   3: timed out
-        },{enableHighAccuracy:true,maximumAge:0});
-    }
+/*getLocation = function() {
+    var position;
+    navigator.geolocation.getCurrentPosition(function(pos) {
+        //document.getElementById('currentLat').innerHTML = position.coords.latitude.toString();
+        //document.getElementById('currentLong').innerHTML = position.coords.longitude.toString();
+        position = pos;
+        console.log(position);
+    }, function(error) {
+        //alert('Error occurred. Error code: ' + error.code);
+        if(error.code == 1){
+            alert("Please turn on Location Services and GPS, and allow your app to use them.");
+        }
+        if(error.code == 2){
+            //Location unavailable - you go into gray mode.
+        }
+        // error.code can be:
+        //   0: unknown error
+        //   1: permission denied
+        //   2: position unavailable (error response from location provider)
+        //   3: timed out
+    },{enableHighAccuracy:true,maximumAge:0});
+    return position;
+}*/
+watchPosition = function() {
+    navigator.geolocation.getCurrentPosition(function(position) {
+        setPosition(position.coords.latitude, position.coords.longitude, position.coords.speed, position.coords.heading, position.coords.accuracy);
+    }, function(error) {
+        //alert('Error occurred. Error code: ' + error.code);
+        if(error.code == 1){
+            alert("Please turn on Location Services and GPS, and allow your app to use them.");
+        }
+        if(error.code == 2){
+            //Location unavailable - you go into gray mode.
+        }
+        // error.code can be:
+        //   0: unknown error
+        //   1: permission denied
+        //   2: position unavailable (error response from location provider)
+        //   3: timed out
+    },{enableHighAccuracy:true,maximumAge:0});
+    return 'Location set.';
+}
+setPosition = function(lat, long, speed, heading, accuracy) {
+    console.log(lat,long,speed,heading,accuracy);
+    Meteor.call('updatePlayer', Session.get('id'), lat, long, speed, heading, accuracy);
+}
     function checkOnline(){
         try{
         var myRequest = new XMLHttpRequest();
